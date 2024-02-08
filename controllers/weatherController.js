b21744cc2f557203557d58ce63b1828cad698b84
Devi-CodingTime <devipurnima26899@gmail.com>
2024-02-08 15:29:24 +0530

    jvhfht

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -4,11 +4,14 @@ const getWeatherByCityName = async (req, res) => {
   const cityName = req.params.cityName;
 
   try {
+    const weatherData = await findOne({cityName});
+    if(!weatherData)
+    return res.status(404).json({ message: 'Weather data not found for the given city' });
+
     // TODO: Implement logic to retrieve weather data by city name
     // Example response when data is found:
-    // res.status(200).json({ weatherData });
+    return res.status(200).json({ weatherData });
     // Example response when data is not found:
-    // res.status(404).json({ message: 'Weather data not found for the given city' });
   } catch (error) {
     res
       .status(500)
@@ -20,11 +23,14 @@ const getWeatherByZipCode = async (req, res) => {
   const zipCode = req.params.zipCode;
 
   try {
+    const weatherData = await findOne({zipCode});
+    if(!weatherData)
+    return res.status(404).json({ message: 'Weather data not found for the given zip code' });
+
     // TODO: Implement logic to retrieve weather data by zip code
     // Example response when data is found:
-    // res.status(200).json({ weatherData });
+   return res.status(200).json({ weatherData });
     // Example response when data is not found:
-    // res.status(404).json({ message: 'Weather data not found for the given zip code' });
   } catch (error) {
     res
       .status(500)
@@ -37,9 +43,10 @@ const postWeatherAlert = async (req, res) => {
     req.body;
 
   try {
+    const newAlert = await WeatherData.create({ cityName, humidity, weatherDescription, temperature, zipCode });
     // TODO: Implement logic to post weather alert
     // Example response when alert is posted successfully:
-    // res.status(201).json({ message: 'Weather alert posted successfully', alert: newAlert });
+    res.status(201).json({ message: 'Weather alert posted successfully', alert: newAlert });
   } catch (error) {
     res
       .status(500)
--- models/weatherDataModel.js
+++ models/weatherDataModel.js
@@ -2,6 +2,26 @@ const mongoose = require('mongoose');
 
 const weatherDataSchema = new mongoose.Schema({
   // City name (Type: String, Required: true)
+  cityName:{
+    type:String,
+    required:true
+  },
+  temperature:{
+    type:String,
+    required:true
+  },
+  zipCode:{
+    type:String,
+    required:true
+  },
+  humidity:{
+    type:Number,
+    required:true
+  },
+  weatherDescription:{
+    type:String,
+    required:true
+  }
   // Temperature in degrees (Type: Number, Required: true)
   // Zip code (Type: String, Required: true)
   // Humidity percentage (Type: Number, Required: true)
